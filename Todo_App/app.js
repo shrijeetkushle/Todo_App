5c0a5be0a260ec6d9ee91f09836092807c584483
Shrijeet Kushle <145208495+shrijeetkushle@users.noreply.github.com>
2025-12-06 18:35:37 +0530

    Add files via upload

---
+++ Todo_App/app.js
@@ -0,0 +1,36 @@
+let todo=[];
+
+let req=prompt("Enter the Request :");
+
+while(true){
+
+    if(req=="quit"){
+        console.log("qutting App!");
+        break;
+    }
+
+    if(req=="list"){
+
+        console.log("---------------");
+        for(let i=0;i<todo.length;i++){
+            console.log(i,todo[i]);
+        }
+        console.log("---------------");
+
+    }else if(req=="add"){
+
+        let add=prompt("Enter the task to Add :");
+        todo.push(add);
+        console.log("Task Added!");
+
+    }else if(req=="delete"){
+
+        let idx=prompt("Enter the idx Number to delete :");
+        todo.splice(idx,1);
+        console.log("Task deleted!");
+    }else{
+        console.log("Wrong Request!");
+    }
+
+    req=prompt("Enter the Request :");
+}
